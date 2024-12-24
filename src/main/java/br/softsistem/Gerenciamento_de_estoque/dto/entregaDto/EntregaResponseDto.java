720d7406e7a3449fb2007a1611f091a57fffaaee
Agster <agster.santos01@gmail.com>
2024-12-23 21:15:15 -0300

    correção dto entregaResponse

--- src/main/java/br/softsistem/Gerenciamento_de_estoque/dto/entregaDto/EntregaResponseDto.java
+++ src/main/java/br/softsistem/Gerenciamento_de_estoque/dto/entregaDto/EntregaResponseDto.java
@@ -4,25 +4,15 @@ import br.softsistem.Gerenciamento_de_estoque.model.Entrega;
 
 import java.time.LocalDateTime;
 
-public class EntregaResponseDto {
-    private Long id;
-    private String nomeConsumidor;
-    private String nomeProduto;
-    private String nomeEntregador;
-    private Integer quantidade;
-    private LocalDateTime horarioEntrega;
-
-    // Construtor
-    public EntregaResponseDto(Long id, String nomeConsumidor, String nomeProduto, String nomeEntregador, Integer quantidade, LocalDateTime horarioEntrega) {
-        this.id = id;
-        this.nomeConsumidor = nomeConsumidor;
-        this.nomeProduto = nomeProduto;
-        this.nomeEntregador = nomeEntregador;
-        this.quantidade = quantidade;
-        this.horarioEntrega = horarioEntrega;
-    }
-
-    // Método estático para conversão
+public record EntregaResponseDto(
+        Long id,
+        String nomeConsumidor,
+        String nomeProduto,
+        String nomeEntregador,
+        Integer quantidade,
+        LocalDateTime horarioEntrega
+) {
+    // Método estático para conversão de entidade para DTO
     public static EntregaResponseDto fromEntity(Entrega entrega) {
         return new EntregaResponseDto(
                 entrega.getId(),
